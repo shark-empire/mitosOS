e8d86dd15c0e0650c6930c6c0b11c0722f5386a7
Shark-empire <babor3331@icloud.com>
2026-07-21 04:37:35 +0000

    Create mod.rs

---
+++ src/fs/mod.rs
@@ -0,0 +1,25 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+pub enum NodeType {
+    File,
+    Directory,
+}
+
+pub struct Metadata {
+    pub name: String,
+    pub size: usize,
+    pub node_type: NodeType,
+}
+
+pub trait FileNode {
+    fn metadata(&self) -> Metadata;
+    fn read(&self, offset: usize, buf: &mut [u8]) -> Result<usize, &'static str>;
+    // Future write support for writeable filesystems like FAT32:
+    // fn write(&mut self, offset: usize, buf: &[u8]) -> Result<usize, &'static str>;
+}
+
+pub trait FileSystem: Send + Sync {
+    fn root(&self) -> alloc::sync::Arc<dyn FileNode>;
+    fn lookup(&self, path: &str) -> Option<alloc::sync::Arc<dyn FileNode>>;
+}
