abde4f1deb9b2145f06829870bcfdb6a2412fd40
Shark-empire <babor3331@icloud.com>
2026-07-24 21:21:04 +0000

    Update pci.rs

--- src/pci.rs
+++ src/pci.rs
@@ -125,7 +125,7 @@ pub fn scan_pci_devices(uart: &mut impl Write) {
             let _ = writeln!(uart, "Failed to initialize AHCI controller: {:?}", e);
         }
      
-   }
+   
  
 
 // --- Enumerator ---
