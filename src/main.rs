10718c2546e0e4c41f84eb3c41874001a5665249
Shark-empire <babor3331@icloud.com>
2026-07-24 22:02:57 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -91,43 +91,7 @@ if let Some(frame) = crate::memory::alloc_frame() {
     
     
 
-// Remove the frame_allocator field from the struct!
-pub struct KernelHal {
-    pub phys_mem_offset: u64,
-}
 
-impl crate::drivers::ahci::Hal for KernelHal {
-    unsafe fn map_mmio(&mut self, phys:  PhysAddr, _size: usize) -> VirtAddr {
-         VirtAddr::new(phys.as_u64() + self.phys_mem_offset)
-    }
-
-    unsafe fn alloc_dma(&mut self, size: usize) -> Option<(PhysAddr, VirtAddr)> {
-        let frames_needed = (size + 4095) / 4096;
-        
-        // Use your global PMM directly instead of a struct field
-        let mut pmm = crate::memory::PHYSICAL_PMM.lock();
-        let first_frame_idx = pmm.allocate_next_frame()?;
-        let start_phys = PhysAddr::new((first_frame_idx * 4096) as u64);
-
-        let virt = VirtAddr::new(start_phys.as_u64() + self.phys_mem_offset);
-        
-        // Zero out the DMA memory block
-        unsafe{
-        core::ptr::write_bytes(virt.as_u64() as *mut u8, 0, frames_needed * 4096);
-        }
-        Some((start_phys, virt))
-    }
-
-    unsafe fn virt_to_phys(&self, virt: VirtAddr) -> Option<PhysAddr> {
-        Some(PhysAddr::new(virt.as_u64() - self.phys_mem_offset))
-    }
-
-    fn wait_micros(&self, micros: u32) {
-        for _ in 0..(micros as u64 * 1000) {
-            core::hint::spin_loop();
-        }
-    }
-}
 
 
 // Inside src/main.rs
