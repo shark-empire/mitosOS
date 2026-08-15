881a5c70f494f5dd5498d99b5d33e724698c88d5
Shark-empire <babor3331@icloud.com>
2026-08-15 10:07:20 +0000

    Refactor UART logging in HAL initialization

--- src/hal/mod.rs
+++ src/hal/mod.rs
@@ -4,35 +4,66 @@ pub mod acpi;
 
 pub fn init() {
     let mut uart = crate::uart::Uart::shared();
-    let _ = core::fmt::Write::write_str(&mut uart, "mitosOS: Initializing Hardware Abstraction Layer...\n");
+
+    let _ = core::fmt::Write::write_str(
+        &mut uart,
+        "mitosOS: Initializing Hardware Abstraction Layer...\n",
+    );
 
     match acpi::get_limine_rsdp() {
         Ok(rsdp_addr) => {
             let _ = core::fmt::Write::write_fmt(
                 &mut uart,
-                format_args!("mitosOS: Limine ACPI RSDP located at virtual 0x{:X}\n", rsdp_addr)
+                format_args!(
+                    "mitosOS: Limine ACPI RSDP located at virtual 0x{:X}\n",
+                    rsdp_addr
+                ),
             );
-            
+
             match acpi::parse_rsdp(rsdp_addr) {
-                Ok(root_table_addr) => {
-                    let _ = core::fmt::Write::write_fmt(
-                        &mut uart,
-                        format_args!("mitosOS: ACPI Root Table (RSDT/XSDT) at physical 0x{:X}\n", root_table_addr)
-                    );
-                    // Next step will be parsing this root table for the MADT and MCFG
+                Ok(root_table) => {
+                    match root_table {
+                        acpi::RootTable::Xsdt(addr) => {
+                            let _ = core::fmt::Write::write_fmt(
+                                &mut uart,
+                                format_args!(
+                                    "mitosOS: ACPI 2.0+ XSDT at physical 0x{:X}\n",
+                                    addr
+                                ),
+                            );
+                        }
+
+                        acpi::RootTable::Rsdt(addr) => {
+                            let _ = core::fmt::Write::write_fmt(
+                                &mut uart,
+                                format_args!(
+                                    "mitosOS: ACPI 1.0 RSDT at physical 0x{:X}\n",
+                                    addr
+                                ),
+                            );
+                        }
+                    }
                 }
+
                 Err(e) => {
                     let _ = core::fmt::Write::write_fmt(
                         &mut uart,
-                        format_args!("mitosOS: ERR: Failed to parse RSDP - {}\n", e)
+                        format_args!(
+                            "mitosOS: ERR: Failed to parse RSDP - {}\n",
+                            e
+                        ),
                     );
                 }
             }
-        },
+        }
+
         Err(e) => {
             let _ = core::fmt::Write::write_fmt(
                 &mut uart,
-                format_args!("mitosOS: ERR: ACPI RSDP not found via Limine - {}\n", e)
+                format_args!(
+                    "mitosOS: ERR: ACPI RSDP not found via Limine - {}\n",
+                    e
+                ),
             );
         }
     }
